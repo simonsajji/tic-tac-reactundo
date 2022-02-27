e53259e38dde85777fa06fbb3a2cce1a18ed950a
simonsajji <simonmathai777@gmail.com>
2022-02-27 17:47:22 +0530

    undo with react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Game />
+        
+      </>
+    );
+  }
+}
+
+// export default App;
---
+++ src/Square.js
@@ -0,0 +1,13 @@
+class Square extends React.Component{
+
+    render(){
+        return (
+            <button className="square" onClick={this.props.onClick}>
+                {this.props.value}
+            </button>
+        )
+
+
+    }
+  
+}
